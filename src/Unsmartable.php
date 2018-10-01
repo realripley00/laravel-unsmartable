255df3248b8cf834704429a962ce705d03500d4c
Michael Burgin <realripley00@gmail.com>
2018-09-30 21:09:34 -0400

    Remove double spacing and Microsoft specific characters.

--- src/Unsmartable.php
+++ src/Unsmartable.php
@@ -12,5 +12,25 @@ trait Unsmartable
 	 */
 	public static function bootUnsmartable()
 	{
-	}
+
+    }
+    
+    public function unSmart($content)
+    {
+        $content = preg_replace('/\s+/', ' ',  $content); 
+
+        $search = array(chr(145),
+                    chr(146),
+                    chr(147),
+                    chr(148),
+                    chr(151));
+ 
+        $replace = array("'",
+                        "'",
+                        '"',
+                        '"',
+                        '-');
+    
+        return str_replace($search, $replace, $content); 
+    }
 }
